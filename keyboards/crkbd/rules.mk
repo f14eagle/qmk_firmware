4b42a1e4de884e107eaf8da4fd6beeb69e7deaab
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 17:43:43 +0800

    add corne template

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -26,7 +26,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
@@ -37,6 +37,6 @@ QUANTUM_LIB_SRC += 	i2c.c \
 SRC += ssd1306.c
 
 # if firmware size over limit, try this option
-# CFLAGS += -flto
+CFLAGS += -flto
 
 DEFAULT_FOLDER = crkbd/rev1
