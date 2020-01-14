178f21de1cad3f821408ef4cd3748f86f7b322aa
BuildTools <unconfigured@null.spigotmc.org>
2020-01-14 20:40:38 +0800

    update firmware

--- keyboards/redox_test/config.h
+++ keyboards/redox_test/config.h
@@ -30,13 +30,13 @@
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 18
-#define PAD_START_COL 15
+#define PAD_START_COL 14
 
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { B5, B5, B5, B5, B5, B5, B5, B5, B5, B5, B5, B5, B5, F6, F7, B1, B3, B2 }
+#define MATRIX_COL_PINS { B6, B6, B6, B6, B6, B6, B6, B6, B6, B6, B6, B6, B6, B6, F6, F7, B1, B3 }
 
-#define RGB_DI_PIN B6
-#define RGBLED_NUM 12
+#define RGB_DI_PIN B5
+#define RGBLED_NUM 16
 
 #define RGBLIGHT_ANIMATIONS
 /* define if matrix has ghost */
--- keyboards/redox_test/matrix.c
+++ keyboards/redox_test/matrix.c
@@ -94,57 +94,50 @@ uint8_t pad_start_col(void){
     return PAD_START_COL;
 }
 
-static void select_col(uint8_t col) {
-    setPinOutput(col_pins[col]);
-    writePinLow(col_pins[col]);
+static void select_row(uint8_t row) {
+    setPinOutput(row_pins[row]);
+    writePinLow(row_pins[row]);
 }
 
-static void unselect_col(uint8_t col) { setPinInputHigh(col_pins[col]); }
+static void unselect_row(uint8_t row) { setPinInputHigh(row_pins[row]); }
 
-static void unselect_cols(void) {
-    for (uint8_t x = PAD_START_COL; x < MATRIX_COLS; x++) {
-        setPinInputHigh(col_pins[x]);
-    }
-}
-
-static void init_pins(void) {
-    unselect_cols();
+static void unselect_rows(void) {
     for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
         setPinInputHigh(row_pins[x]);
     }
 }
 
-static bool read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col) {
-    bool matrix_changed = false;
+static void init_pins(void) {
+    unselect_rows();
+    for (uint8_t x = PAD_START_COL; x < MATRIX_COLS; x++) {
+        setPinInputHigh(col_pins[x]);
+    }
+}
 
-    // Select col and wait for col selecton to stabilize
-    select_col(current_col);
+static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
+    // Store last value of row prior to reading
+    matrix_row_t last_row_value = current_matrix[current_row];
+
+    // Clear data in matrix row
+    current_matrix[current_row] = 0;
+
+    // Select row and wait for row selecton to stabilize
+    select_row(current_row);
     wait_us(30);
 
-    // For each row...
-    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
-        // Store last value of row prior to reading
-        matrix_row_t last_row_value = current_matrix[row_index];
+    // For each col...
+    for (uint8_t col_index = PAD_START_COL; col_index < MATRIX_COLS; col_index++) {
+        // Select the col pin to read (active low)
+        uint8_t pin_state = readPin(col_pins[col_index]);
 
-        // Check row pin state
-        if (readPin(row_pins[row_index]) == 0) {
-            // Pin LO, set col bit
-            current_matrix[row_index] |= (ROW_SHIFTER << current_col);
-        } else {
-            // Pin HI, clear col bit
-            current_matrix[row_index] &= ~(ROW_SHIFTER << current_col);
-        }
-
-        // Determine if the matrix changed state
-        if ((last_row_value != current_matrix[row_index]) && !(matrix_changed)) {
-            matrix_changed = true;
-        }
+        // Populate the matrix row with the state of the col pin
+        current_matrix[current_row] |= pin_state ? 0 : (ROW_SHIFTER << col_index);
     }
 
-    // Unselect col
-    unselect_col(current_col);
+    // Unselect row
+    unselect_row(current_row);
 
-    return matrix_changed;
+    return (last_row_value != current_matrix[current_row]);
 }
 
 void matrix_init(void) {
@@ -201,8 +194,8 @@ uint8_t matrix_scan(void)
 
     // Wire logic
     bool changed = false;
-    for (uint8_t current_col = PAD_START_COL; current_col < MATRIX_COLS; current_col++) {
-        changed |= read_rows_on_col(raw_matrix, current_col);
+    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
+        changed |= read_cols_on_row(raw_matrix, current_row);
     }
     debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
 
